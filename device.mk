0fda8185a9e2484e29e2d18161b76082c4fd0c44
Kalpaj Chaudhari <daedroza@gmail.com>
2023-01-07 09:18:52 +0530

    dubai: Build with GMS
    Change-Id: I9d519df030c5e70648ff95092578cc3d12853818

--- device.mk
+++ device.mk
@@ -100,3 +100,4 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/dubai/dubai-vendor.mk)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
